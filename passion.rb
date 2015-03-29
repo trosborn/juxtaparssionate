c4fb868267649c377927382701955a1de873ea61
Thomas Osborn <trosborn@gmail.com>
2015-03-29 00:18:16 -0700

    collects data from div.listing-container

--- passion.rb
+++ passion.rb
@@ -2,18 +2,14 @@ require 'mechanize'
 require 'nokogiri'
 
 agent = Mechanize.new
-posting = Mechanize.new
-
-page = agent.get('https://weworkremotely.com/categories/2/jobs')
-
-page.links_with( :href => %r{/jobs/} ).each do |link|
-  next if link.href == 'https://weworkremotely.com/jobs/new'
-  puts link.href
-end
 
 def file
-  f = File.open('Passion.txt')
-  file = f.read
-  no_new_lines = file.gsub("/n", '')
+  page = agent.get('https://weworkremotely.com/categories/2/jobs')
+  page.links_with( :href => %r{/jobs/} ).each do |link|
+    next if link.href == 'https://weworkremotely.com/jobs/new'
+    page = link.click
+    doc = page.parser
+    puts doc.css('div.listing-container').to_s.gsub('<', ' ').gsub('>', ' ')
+  end
 end
 
